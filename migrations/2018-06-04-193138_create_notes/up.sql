436e3e805da8a50c308ead75c004fccd620b1083
Mishio595 <tenseiken595@gmail.com>
2018-08-03 17:19:18 -0600

    Forgot to add this with the last commit

--- migrations/2018-06-04-193138_create_notes/up.sql
+++ migrations/2018-06-04-193138_create_notes/up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE notes (
-	index SERIAL PRIMARY KEY,
+	index SERIAL NOT NULL,
 	user_id BIGINT NOT NULL,
 	guild_id BIGINT NOT NULL,
 	note TEXT NOT NULL,
 	moderator BIGINT NOT NULL,
 	timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+	PRIMARY KEY(index, user_id, guild_id)
 )
