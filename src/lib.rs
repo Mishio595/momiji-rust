f130f14be0c3fbd58d388d760a1535c89af26841
Mishio595 <tenseiken595@gmail.com>
2018-08-25 20:38:16 -0600

    Split client to own mod

--- src/lib.rs
+++ src/lib.rs
@@ -27,67 +27,6 @@ pub mod macros;
 pub mod core;
 pub mod db;
 pub mod modules;
+pub mod momiji_client;
 
-use self::core::{
-    api,
-    handler::Handler,
-    model::*,
-    framework::MomijiFramework,
-    timers
-};
-use serenity::Error as SerenityError;
-use serenity::http;
-use serenity::model::id::UserId;
-use serenity::prelude::{
-    Client,
-    Mutex,
-};
-use std::collections::HashSet;
-use std::env;
-use std::sync::Arc;
-
-pub struct MomijiClient(Client);
-
-impl MomijiClient {
-    pub fn new() -> Self {
-        let token = env::var("DISCORD_TOKEN").expect("Expected token in environment");
-        let mut client = Client::new(&token, Handler).expect("Unable to initialize client");
-        {
-            let mut data = client.data.lock();
-            let api_client = api::ApiClient::new();
-            let tc = timers::TimerClient::new();
-            data.insert::<SerenityShardManager>(Arc::clone(&client.shard_manager));
-            data.insert::<ApiClient>(api_client);
-            data.insert::<TC>(Arc::new(Mutex::new(tc)));
-        }
-        let owners = match http::get_current_application_info() {
-            Ok(info) => {
-                let mut set = HashSet::new();
-                let mut data = client.data.lock();
-                data.insert::<Owner>(info.owner.id);
-                set.insert(info.owner.id);
-                set
-            },
-            Err(why) => panic!("Couldn't get the application info: {:?}", why),
-        };
-        client.with_framework(MomijiFramework::new(owners));
-        MomijiClient(client)
-    }
-
-    pub fn new_with_owners(owners: HashSet<UserId>) -> Self {
-        let token = env::var("DISCORD_TOKEN").expect("Expected token in environment");
-        let mut client = Client::new(&token, Handler).expect("Unable to initialize client");
-        {
-            let mut data = client.data.lock();
-            let api_client = api::ApiClient::new();
-            let tc = timers::TimerClient::new();
-            data.insert::<SerenityShardManager>(Arc::clone(&client.shard_manager));
-            data.insert::<ApiClient>(api_client);
-            data.insert::<TC>(Arc::new(Mutex::new(tc)));
-        }
-        client.with_framework(MomijiFramework::new(owners));
-        MomijiClient(client)
-    }
-
-    pub fn start_autosharded(&mut self) -> Result<(), SerenityError> { self.0.start_autosharded() }
-}
+pub use momiji_client::MomijiClient;
