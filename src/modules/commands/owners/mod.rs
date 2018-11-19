6ab4ce1717c253fa5b1318f1c255a05116f8d3c6
Mishio595 <tenseiken595@gmail.com>
2018-11-18 19:50:41 -0700

    Hide owner-only commands from help menu

--- src/modules/commands/owners/mod.rs
+++ src/modules/commands/owners/mod.rs
@@ -7,6 +7,7 @@ use serenity::framework::standard::CreateGroup;
 
 pub fn init() -> CreateGroup {
     CreateGroup::default()
+        .help_available(false)
         .cmd("log", Log)
         .cmd("op", Premium)
 }
