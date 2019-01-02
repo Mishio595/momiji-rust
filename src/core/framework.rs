e4347e37757690649101d9f5cac4ea25a55cc9fa
Adelyn Breelove <tenseiken595@gmail.com>
2019-01-02 09:15:08 -0700

    fix some formatting

--- src/core/framework.rs
+++ src/core/framework.rs
@@ -118,12 +118,28 @@ impl MomijiFramework {
             .on_dispatch_error(|_, message, error| {
                 use serenity::framework::standard::DispatchError;
                 match error {
-                    DispatchError::LackOfPermissions(perm) => check_error!(message.channel_id.say(format!("You lack the following permissions needed to execute this command: {:?}", perm))),
-                    DispatchError::RateLimited(time) => check_error!(message.channel_id.say(format!("A bit too soon for that. Please wait {} seconds before trying again.", time))),
-                    DispatchError::NotEnoughArguments { min, given } => check_error!(message.channel_id.say(format!("Too few arguments provided. {} given, {} minimum.", given, min))),
-                    DispatchError::TooManyArguments { max, given } => check_error!(message.channel_id.say(format!("Too many arguments provided. {} given, {} maximum.", given, max))),
-                    DispatchError::OnlyForDM => check_error!(message.channel_id.say("This command is only available in private channels.")),
-                    DispatchError::OnlyForGuilds => check_error!(message.channel_id.say("This command is only available in guilds.")),
+                    DispatchError::LackOfPermissions(perm) => check_error!(
+                        message.channel_id.say(format!(
+                            "You lack the following permissions needed to execute this command: {:?}"
+                            ,perm))),
+                    DispatchError::RateLimited(time) => check_error!(
+                        message.channel_id.say(format!(
+                            "A bit too soon for that. Please wait {} seconds before trying again."
+                            ,time))),
+                    DispatchError::NotEnoughArguments { min, given } => check_error!(
+                        message.channel_id.say(format!(
+                            "Too few arguments provided. {} given, {} minimum."
+                            ,given
+                            ,min))),
+                    DispatchError::TooManyArguments { max, given } => check_error!(
+                        message.channel_id.say(format!(
+                            "Too many arguments provided. {} given, {} maximum."
+                            ,given
+                            ,max))),
+                    DispatchError::OnlyForDM => check_error!(
+                        message.channel_id.say("This command is only available in private channels.")),
+                    DispatchError::OnlyForGuilds => check_error!(
+                        message.channel_id.say("This command is only available in guilds.")),
                     _ => (),
                 }
             })
